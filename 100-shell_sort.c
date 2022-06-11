b61a451cdeab5023394f91a081f6ae6a60fb46ff
ezesilva95 <4381@holbertonschool.com>
2022-06-11 13:35:22 -0700

    Task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,7 +12,7 @@ void shell_sort(int *array, size_t size)
 	if (array == NULL)
 		return;
 
-	while (interval < size)
+	while (interval < size / 3)
 		interval = interval * 3 + 1;
 
 	while (interval)
